e7a30d8852d775a8e61bca71f5947f47906b4eef
eblee9o <hello.eblee@gmail.com>
2025-09-14 09:20:17 +0000

    vars edit

--- docker-demo-3/s3.tf
+++ docker-demo-3/s3.tf
@@ -1,20 +1,3 @@
-########################
-# s3.tf (생성 금지 가드)
-########################
-
-# 백엔드 버킷은 "참조"만 하고, 필요할 때만 생성
-variable "create_backend_bucket" {
-  description = "백엔드용 S3 버킷을 여기서 직접 생성할지 여부(기본: 생성 안 함)"
-  type        = bool
-  default     = false
-}
-
-variable "backend_bucket_name" {
-  description = "이미 존재하는 S3 버킷 이름 (예: terraform-state-a2b6219)"
-  type        = string
-}
-
-# 필요할 때만 생성 (대부분 환경에선 count=0 → 생성 안 함)
 resource "aws_s3_bucket" "terraform_state" {
   count  = var.create_backend_bucket ? 1 : 0
   bucket = var.backend_bucket_name

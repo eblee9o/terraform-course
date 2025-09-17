9605ff5ebde4a5f0fa8dfb5f3f6ae6ca6c34644b
eblee9o <hello.eblee@gmail.com>
2025-09-17 03:56:52 +0000

    iam edit

--- docker-demo-3/iam.tf
+++ docker-demo-3/iam.tf
@@ -1,22 +1,12 @@
 ########################################
-# Toggle: IAM 생성 여부 (권한 없으면 false)
+# ECS Service Role (create or reference)
+# - 생성 여부: var.create_service_roles (vars.tf에 있음; 기본 false)
+# - 역할 이름 : var.ecs_service_role_name (vars.tf에 있음; 기본 "ecs-service-role")
 ########################################
-variable "create_ecs_service_role" {
-  type    = bool
-  default = true
-}
 
-# 이미 사용 중인 변수/locals와 호환
-variable "ecs_service_role_name" {
-  type    = string
-  default = "ecs-service-role"
-}
-
-########################################
-# (A) 생성 모드
-########################################
+# 생성 모드
 resource "aws_iam_role" "ecs_service_role" {
-  count = var.create_ecs_service_role ? 1 : 0
+  count = var.create_service_roles ? 1 : 0
 
   name = var.ecs_service_role_name
   assume_role_policy = jsonencode({
@@ -30,28 +20,21 @@ resource "aws_iam_role" "ecs_service_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_service_role_attach" {
-  count      = var.create_ecs_service_role ? 1 : 0
+  count      = var.create_service_roles ? 1 : 0
   role       = aws_iam_role.ecs_service_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
-########################################
-# (B) 참조 모드 (이미 있는 역할만 사용)
-########################################
+# 참조 모드(이미 있는 역할만 사용)
 data "aws_iam_role" "ecs_service_role" {
-  count = var.create_ecs_service_role ? 0 : 1
+  count = var.create_service_roles ? 0 : 1
   name  = var.ecs_service_role_name
 }
 
-########################################
-# 공통 값 (삼항은 한 줄로!)
-########################################
+# 공통 출력(둘 중 무엇을 쓰든 한 경로로 소비)
 locals {
-  ecs_service_role_name_effective = var.create_ecs_service_role ? aws_iam_role.ecs_service_role[0].name : data.aws_iam_role.ecs_service_role[0].name
-  ecs_service_role_arn_effective  = var.create_ecs_service_role ? aws_iam_role.ecs_service_role[0].arn  : data.aws_iam_role.ecs_service_role[0].arn
-
-  # 기존 코드 호환: 너의 iam.tf에서 쓰던 local 이름으로도 노출
-  ecs_service_role_name = local.ecs_service_role_name_effective
+  ecs_service_role_name_effective = var.create_service_roles ? aws_iam_role.ecs_service_role[0].name : data.aws_iam_role.ecs_service_role[0].name
+  ecs_service_role_arn_effective  = var.create_service_roles ? aws_iam_role.ecs_service_role[0].arn  : data.aws_iam_role.ecs_service_role[0].arn
 }
 
 output "ecs_service_role_name_effective" {
